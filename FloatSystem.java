8382e460fffecf665a132d02cd4fd9af905320ad
lakshmitha8 <89855709+lakshmitha8@users.noreply.github.com>
2021-09-08 12:14:59 +0530

    Add files via upload

---
+++ FloatSystem.java
@@ -0,0 +1,20 @@
+class FloatSystem{
+	public static void main(String args[]){
+	float marks;
+	marks=57.97f;
+	System.out.println("marks "+marks);
+	
+	float a;
+	a=6.23f;
+	
+	float weight;
+	weight=70.89f;
+	
+	float salary;
+	salary=250000.00f;
+	
+	System.out.println("the heightis "+a);
+	System.out.println("weight "+weight);
+	System.out.println("salary "+salary);
+	}
+}
---
+++ LongExample.java
@@ -0,0 +1,13 @@
+class LongExample
+{
+	public static void main(String args[]){
+	long phonenum;
+	phonenum=123456789;
+	
+	long calculatornum;
+	calculatornum=9876553;
+	
+	System.out.println("phonenum is "+phonenum);
+	System.out.println("calculatornum is "+calculatornum);
+	}
+}
