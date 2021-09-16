2eb24f9009fb1b1450fd6edabfeffc0599dda17d
lakshmitha8 <89855709+lakshmitha8@users.noreply.github.com>
2021-09-16 22:00:25 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,14 @@
+
+public class Divisible {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for(int i=0; i<20; i++) {
+			if(i%3==0) {
+				System.out.println(i);
+			}
+		}
+
+}
+}
